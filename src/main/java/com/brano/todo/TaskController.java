cfc957490352b33a5ace6835fc49b07a2e589c4f
Branislav Goga <brano.goga@centrum.sk>
2021-12-19 20:53:54 +0100

    Static HTML page

---
+++ src/main/java/com/brano/todo/TaskController.java
@@ -0,0 +1,14 @@
+package com.brano.todo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+
+	@GetMapping(value={"/", "/list"})
+	public String list(Model model) {
+		return "list";
+	}
+}
