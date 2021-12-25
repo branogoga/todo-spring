0fe1b7d65e7eb372e96272aa7044a691019433f8
Branislav Goga <brano.goga@centrum.sk>
2021-12-25 10:22:49 +0100

    Show list of tasks from model

--- src/main/java/com/brano/todo/TaskController.java
+++ src/main/java/com/brano/todo/TaskController.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class TaskController {
 
 	@GetMapping(value={"/", "/list"})
 	public String list(Model model) {
-		Task task = new Task("Foo Task", "Foo Description");
-		model.addAttribute("task", task);
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task("Task 1", "Description 1"));
+		tasks.add(new Task("Task 2", "Description 2"));
+		model.addAttribute("tasks", tasks);
 		return "list";
 	}
 }
